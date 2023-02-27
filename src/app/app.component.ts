0fc7afff5cd0579569cbb75f04bab711359d0a58
Sam VH <svanharen@my.bcit.ca>
2023-02-27 12:18:17 -0800

    inital push - angular assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,35 @@
 import { Component } from '@angular/core';
 
+export class Category {
+  id!: number;
+  name!: string;
+  slug!: string;
+}
+
+export class Tag {
+  id!: number;
+  name!: string;
+  slug!: string;
+  pivot?: any;
+}
+
+export class Project {
+  'id': number;
+  'title': string;
+  'slug': string;
+  'excerpt': string;
+  'body': string;
+  'url': string | null;
+  'published_date': string | null;
+  'image': string | null;
+  'thumb': string | null;
+  'category_id': number | null;
+  'created_at': string;
+  'updated_at': string;
+  'category': Category;
+  'tags': Tag[] | undefined;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
